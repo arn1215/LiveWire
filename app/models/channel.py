9d3c68cfb88990355f26bcc7ea4dae503a2bae72
Ali <ali.naqvi.dev@gmail.com>
2022-04-04 00:01:01 -0500

    finished rough message model

---
+++ app/models/channel.py
@@ -0,0 +1,18 @@
+from .db import db
+
+
+class Channel(db.Model):
+    __tablename__ = "channels"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    server_id = db.Column(db.Integer, db.ForeignKey(
+        "server.id"), nullable=False)
+    # Relationships
+    server = db.relationship("Server", back_populates="channel")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'server_id': self.server_id
+        }
