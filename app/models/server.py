aaec3fddff497d4f20265ffdea4084be10d5d123
Ali <ali.naqvi.dev@gmail.com>
2022-04-03 23:50:49 -0500

    Rough Server Model Complete

---
+++ app/models/server.py
@@ -0,0 +1,19 @@
+from .db import db
+
+
+class Server(db.Model):
+    __tablename__ = 'servers'
+    id = db.Column(db.Integer, primary_key=True)
+    owner_id = db.Column(db.Integer, db.ForeignKey("User.id"))
+    name = db.Column(db.String(50))
+    icon = db.Column(db.String(255))
+    # Relationships
+    channel = db.relationship("Channel", back_populates="server")
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'owner_id': self.owner_id,
+            'name': self.name,
+            'icon': self.icon
+        }
