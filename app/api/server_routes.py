dad31bda6aaa8923999720165767465707357c66
Ryan Kagrise <rdkcode@gmail.com>
2022-04-04 10:38:41 -1000

    finished server routes

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -5,18 +5,6 @@ from ..models import db, Server, User, Message, server_users
 server_routes = Blueprint('servers', __name__)
 
 
-# get one server
-@server_routes.route('/<int:server_id>')
-@login_required
-def get_one_server(server_id):
-  server = Server.query.filter(Server.id == server_id).one()
-  return {"server": server.to_dict()}
-
-
-
-
-
-
 # get all servers/create new server
 @server_routes.route('/', methods=['GET', 'POST'])
 def create_post():
@@ -34,3 +22,36 @@ def create_post():
 
   servers = Server.query.all()
   return { "servers": sorted([s.to_dict() for s in servers], key=lambda s: s["id"], reverse=True)}
+
+
+# get one server
+@server_routes.route('/<int:server_id>')
+@login_required
+def get_one_server(server_id):
+  server = Server.query.filter(Server.id == server_id).one()
+  return {"server": server.to_dict()}
+
+
+
+# edit one server
+@server_routes.route('/<int:server_id>')
+@login_required
+def edit_server(server_id):
+  server = Server.query.get(server_id)
+  data = request.json
+  if 'name' in data.keys():
+    server.name = data["name"]
+  if 'icon' in data.keys():
+    server.icon = data["icon"]
+  db.session.commit()
+  return server.to_dict()
+
+
+# delete one server
+@server_routes.route('/<int:server_id>', methods=['DELETE'])
+@login_required
+def delete_server(server_id)
+  server = Server.query.filter(Server.id == server_id).first()
+  db.session.delete(server)
+  db.session.commit()
+  return f"Deleted Server: {server_id}"
