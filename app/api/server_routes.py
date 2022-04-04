ab3818d9a90fe9335d68ba2682a7990812d5f4ab
Ryan Kagrise <rdkcode@gmail.com>
2022-04-04 08:08:51 -1000

    added get_one_server route

--- app/api/server_routes.py
+++ app/api/server_routes.py
@@ -3,8 +3,28 @@ from ..models import db, Server, User, Message
 
 server_routes = Blueprint('servers', __name__)
 
+# get all servers
+@server_routes.route('/')
+@login_required
+def get_all_servers():
+  servers = Server.query.all()
+  return { "servers": sorted([s.to_dict() for s in servers], key=lambda s: s["id"], reverse=True)}
 
-@server_routes.route('/', methods=['GET', 'POST'])
+
+# get one server
+@server_routes.route('/<int:server_id>')
+@login_required
+def get_one_server(server_id):
+  server = Server.query.filter(Server.id == server_id).one()
+  return {"server": server.to_dict()}
+
+
+
+
+
+
+# create new server
+@server_routes.route('/', methods=['POST'])
 def create_post():
 
   if request.method == "POST":
@@ -17,7 +37,3 @@ def create_post():
       db.session.commit()
 
       return server.to_dict()
-
-  servers = Server.query.all()
-
-  return { "servers": sorted([s.to_dict() for s in servers], key=lambda s: s["id"], reverse=True}
