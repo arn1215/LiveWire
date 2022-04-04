b7193f13c777c796929ad3a8f3575abfbc1c7c3d
Ali <ali.naqvi.dev@gmail.com>
2022-04-04 01:28:47 -0500

    added 2 more server seeds

--- app/seeds/servers.py
+++ app/seeds/servers.py
@@ -4,11 +4,18 @@ from app.models import db, Server
 # Adds a demo user, you can add other users here if you want
 def seed_servers():
     demo = Server(
-        id=1, owner_id=1, name='Test server', icon='https://e7.pngegg.com/pngimages/621/237/png-clipart-what-emoji-2-ghost-it-coque-happy-ghost-emoji-text-logo.png', invite_URL='www.hey.com')
+         owner_id=1, name='Test server', icon='https://e7.pngegg.com/pngimages/621/237/png-clipart-what-emoji-2-ghost-it-coque-happy-ghost-emoji-text-logo.png', invite_URL='www.hey.com')
 
+    demo2 = Server(
+         owner_id=1, name='Test server 2', icon='https://e7.pngegg.com/pngimages/621/237/png-clipart-what-emoji-2-ghost-it-coque-happy-ghost-emoji-text-logo.png', invite_URL='www.hey.com')
 
+    demo3 = Server(
+         owner_id=2, name='EGG DOG', icon='https://static.wikia.nocookie.net/meme/images/b/bf/Gfx_eggdog.png/revision/latest/scale-to-width-down/250?cb=20191212182052g', invite_URL='www.hey.com')
+
+    
     db.session.add(demo)
-
+    db.session.add(demo2)
+    db.session.add(demo3)
 
     db.session.commit()
 
