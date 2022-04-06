bf26bc59d4c432ae08264e62a4e7fd1f80683d04
Chris Britton <chrisbritton790@gmail.com>
2022-04-06 13:55:07 -0700

    finished messages routes

--- app/api/message_routes.py
+++ app/api/message_routes.py
@@ -30,11 +30,11 @@ def get_messages(channel_id):
 
 
 # edit one message
-@message_routes.route('/<int:channel_id>', methods=['PUT'])
+@message_routes.route('/<int:message_id>', methods=['PUT'])
 # @login_required
-def edit_message(channel_id):
+def edit_message(message_id):
     data = request.get_json(force=True)
-    message = Message.query.get(data["id"])
+    message = Message.query.get(message_id)
 
     if 'content' in data.keys():
         message.content = data['content']
@@ -44,31 +44,11 @@ def edit_message(channel_id):
 
 
 
-# delete one server
-@server_routes.route('/delete/<int:server_id>', methods=['DELETE'])
+# delete one message
+@message_routes.route('/delete/<int:message_id>', methods=['DELETE'])
 # @login_required
-def delete_server(server_id):
-  server = Server.query.filter(Server.id == server_id).first()
-  db.session.delete(server)
+def delete_message(message_id):
+  message = Message.query.filter(Message.id == message_id).first()
+  db.session.delete(message)
   db.session.commit()
-  return f"Deleted Server: {server_id}"
-
-
-
-# get a user's servers
-@server_routes.route('/byUser/<int:user_id>')
-# @login_required
-def load_on_login(user_id):
-  server_list = Server.query.join(server_users).filter(server_users.c.user_join_id == user_id).all()
-  return {"servers": [server.to_dict() for server in server_list]}
-
-
-# get server by invite
-@server_routes.route('/serverInvite/<server_invite>')
-# @login_required
-def get_server_invite(server_invite):
-  server = Server.query.filter(Server.invite_URL == server_invite).one()
-  if server:
-    return {"server": server.to_dict()}
-  else:
-    return 'Server Does Not Exist!'
+  return f"Deleted Server: {message_id}"
