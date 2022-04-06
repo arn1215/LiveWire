5292086651a01f5e1a4118e462ffede4377e9b79
Ryan Kagrise <rdkcode@gmail.com>
2022-04-05 19:55:03 -1000

    front end sidebar started for channels

--- react-app/src/components/SideBar.js
+++ react-app/src/components/SideBar.js
@@ -1,21 +1,58 @@
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux"
+import { Link } from "react-router-dom"
+import { loadUsersServers } from "../../src/store/server"
 import ChannelCard from './ChannelCard'
 
 const SideBar = () => {
+
+    const dispatch = useDispatch();
+    const servers = useSelector((state) => state.servers)
+    const user = useSelector((state) => state.session.user);
+
+    useEffect(() => {
+        dispatch(loadUsersServers(user.id));
+    }, []);
+
+    if (!servers) {
+        return null
+    }
+
+    // const allServers = Object.values(servers);
+
+    const channelsComponents = user.servers.channel.map((channel) => {
+        return (
+            <>
+                <li key={channel?.id}>
+                    <Link to={`/channels/${channel.id}`}>
+                        {channel?.name}
+                    </Link>
+                </li>
+            </>
+        )
+    });
+
     return (
-        <>
-        
-        
-        <div className="side-bar">
-        <div className="server-info">
-            THIS IS SERVER INFO AND STUFF
-        </div>
-         <ChannelCard />
+        <div className="server-bar">
+            { channelsComponents }
+            </div>
 
-         
-        </div>
-        </>
+//     return (
+//         <>
+
+
+//             <div className="side-bar">
+//                 <div className="server-info">
+//                     THIS IS SERVER INFO AND STUFF
+//                 </div>
+//                 <ChannelCard />
+
+
+//             </div>
+//         </>
+
     )
-}
+};
 
 
-export default SideBar
+export default SideBar;
