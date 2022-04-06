eae8bc88ef39453a1e84cbf74bc749dbb8ea7fcf
Ryan Kagrise <rdkcode@gmail.com>
2022-04-05 19:18:34 -1000

    pushed for chris

--- react-app/src/components/CreateServer/index.js
+++ react-app/src/components/CreateServer/index.js
@@ -7,15 +7,29 @@ function CreateServerModal() {
     const [showModal, setShowModal] = useState(false);
 
     return (
-    <>
-        <button id='buttonCard' className='cs-button' onClick={() => setShowModal(true)}>+</button>
-        {showModal && (
+        <>
+          <button onClick={() => setShowModal(true)}>+</button>
+          {showModal && (
             <Modal onClose={() => setShowModal(false)}>
-                <CreateServer />
+              <CreateServer />
             </Modal>
-        )}
-    </>
-    );
-}
+          )}
+        </>
+      );
+    }
+
+
+
+//     return (
+//     <>
+//         <button id='buttonCard' className='cs-button' onClick={() => setShowModal(true)}>+</button>
+//         {showModal && (
+//             <Modal onClose={() => setShowModal(false)}>
+//                 <CreateServer />
+//             </Modal>
+//         )}
+//     </>
+//     );
+// }
 
 export default CreateServerModal;
