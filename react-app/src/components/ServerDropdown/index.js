d54de2559150e33f5c10fda7e6e91d00360b93cf
Chris Britton <chrisbritton790@gmail.com>
2022-04-07 14:58:49 -0700

    server dropdown ready for testing

--- react-app/src/components/ServerDropdown/index.js
+++ react-app/src/components/ServerDropdown/index.js
@@ -7,6 +7,8 @@ import * as serverActions from '../../store/server';
 const ServerDropdown = () => {
     const dispatch = useDispatch();
     const [showMenu, setShowMenu] = useState(false);
+    const [showNameField, setShowNameField] = useState(false);
+    const [newName, setNewName] = useState('')
     const currentServer = useSelector(state => state.oneServer.server);
     const userServers = useSelector(state => state.userServers)
     const belongsToUser = userServers.find(server => server.name === serverName)
@@ -17,6 +19,17 @@ const ServerDropdown = () => {
         e.preventDefault();
         dispatch(serverActions.removeServer(currentServer.id));
     };
+
+    const openNameField = () => {
+        if (showNameField) return;
+        setShowNameField(true)
+    };
+
+    const handleNameEdit = () => {
+        setShowNameField(false)
+        currentServer.name = newName
+        return dispatch(serverActions.putServer(currentServer))
+    }
 
     const openMenu = () => {
         if (showMenu) return;
@@ -43,6 +56,16 @@ const ServerDropdown = () => {
                 </button>
                 {showMenu && (
                     <div className='server-dropdown'>
+                        <div className='server-name-edit-div' onClick={openNameField}>
+                            Edit Name
+                        </div>
+                        {showNameField && (
+                            <form onSubmit={handleNameEdit}>
+                                <input type='text' value={newName} onChange={e => setNewName(e.target.value)}/>
+                                <button type='submit'>Edit</button>
+                            </form>
+
+                        )}
                         <NavLink to='/server-edit'>Edit Server</NavLink>
                         {belongsToUser && (
                             <div className='delete-server' onClick={deleteServer}>
