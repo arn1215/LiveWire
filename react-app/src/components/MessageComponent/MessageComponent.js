793fe946dd28601873280226b43cb732283446dc
Ali <ali.naqvi.dev@gmail.com>
2022-04-07 17:21:12 -0500

    working on message component

--- react-app/src/components/MessageComponent/MessageComponent.js
+++ react-app/src/components/MessageComponent/MessageComponent.js
@@ -1,35 +1,55 @@
 import { useState } from "react"
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
+import { createMessage } from "../../store/message";
+import './Message.css'
 
 const MessageComponent = () => {
-    
-    
-    
+
+    const dispatch = useDispatch()
+
     const user = useSelector((state) => state.session.user);
+    // const messages = useSelector((state) => state.messages)
+    const messages = [{content: "yo, dude "}, {content: "bruh"}, {content: "wagud "}]
     const channel = useSelector((state) => state.channel)
     const [content, setContent] = useState("")
-    
-    
-    const onSubmit = (e) => {
+
+
+    const onSubmit = async (e) => {
         e.preventDefault()
-        
+
         let message = {
             content,
             message_owner_id: user.id,
-            channel
+            channel_id: channel.id
         }
+        setContent("")
         console.log(message)
+        await dispatch(createMessage(message))
+
     }
     return (
         <>
             <div className="chat">
+            
+               
+              
+                    <div className="message-wrap">
+                        {messages.map(message => (
+                            <div className="message">{message.content}</div>
+                        ))}
+                    </div>
+                   
+           
+                
+                
+
                 <div className="message-card">
                     yo
                 </div>
                 <div className="message-container">
 
                     <form onSubmit={onSubmit}>
-{/* 
+                        {/* 
                         <textarea 
                             placeholder={`Message #`} 
                             type="text"
@@ -40,7 +60,7 @@ const MessageComponent = () => {
                         >
 
                         </textarea> */}
-{/* 
+                        {/* 
                         <button type='submit'>Submit</button> */}
                         <input
                             className="chat-field"
