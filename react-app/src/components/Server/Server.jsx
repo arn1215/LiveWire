1d6bde5d18ae2690ef744e0164e6d9aa9c847d2a
Braxton Kappes <braxtonkappes@gmail.com>
2022-04-09 16:17:06 -0500

    good time to commit working code part 3

--- react-app/src/components/Server/Server.jsx
+++ react-app/src/components/Server/Server.jsx
@@ -2,13 +2,14 @@ import './Server.css'
 import React from 'react'
 import ChannelNav from '../../components/ChannelNav/ChannelNav'
 import MessageComponent from '../../components/MessageComponent/MessageComponent'
+import UserList from '../../components/UserList/UserList'
 
 function Server() {
     return (
         <div className='server'>
             <ChannelNav />
             <MessageComponent />
-            {/* <UserList /> */}
+            <UserList />
         </div>
     )
 }
