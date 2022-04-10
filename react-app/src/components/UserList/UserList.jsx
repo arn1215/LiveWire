7dac2e9067dc996cfa08345e0047f0f315eb3017
Ryan Kagrise <rdkcode@gmail.com>
2022-04-10 11:21:04 -1000

    added userlist and got user to render

--- react-app/src/components/UserList/UserList.jsx
+++ react-app/src/components/UserList/UserList.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
+import React, {useEffect} from 'react'
+import { NavLink, Redirect } from 'react-router-dom'
+import { useSelector, useDispatch } from 'react-redux'
+import { loadServerById } from '../../store/server'
 import './UserList.css'
 
 function UserList() {
+    const dispatch = useDispatch();
+
+    const serverInfo = useSelector((state) => state.server.oneServer)
+    console.log("Server ------------------->", serverInfo)
+    const users = serverInfo.server.users
+    console.log("users------------------------>", users)
+    // const usersArray = Object.values(users)
+
+
     return (
         <div className='user-list'>
-            
+            {users.map((user) => (
+                <div key={user.id} className="username-div">
+                    {user.username}
+                </div>
+            ))}
         </div>
     )
 }
